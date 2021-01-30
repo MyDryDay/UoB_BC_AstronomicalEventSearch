3d80522a3518bb5e501647597139a628d5639007
George Cope <G.Cope13@gmail.com>
2021-01-30 12:07:32 +0000

    Added functionality for modals in the javascript file, sorted formatting for the modal content in the CSS file, & added the appropriate HTML for the modal in index.html

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+    // This initializes the use of the multi-select dropdown menu on the navbar
     $("select").formSelect();
+    // This assigns a value to the selected values (none at this point) and creates an empty array calles values
     var values = $("select").formSelect("getSelectedValues");
+    // This prints the values array to the console
     console.log(values);
+
+    // This initializes the use of the modals for images
+    $('.modal').modal();
 })
