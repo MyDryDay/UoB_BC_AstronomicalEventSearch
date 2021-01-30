8cf2c83f85a1dcbab2cbdc42023f786219f4f959
George Cope <G.Cope13@gmail.com>
2021-01-30 15:10:19 +0000

    Resolving merge errors

--- javascript.js
+++ javascript.js
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
-$(document).ready(function () {
-=======
 $(document).ready(function(){
     // This initializes the use of the multi-select dropdown menu on the navbar
->>>>>>> 0b9cc490311191e6b3ee3e031c147a20673eeadc
     $("select").formSelect();
     // This assigns a value to the selected values (none at this point) and creates an empty array calles values
     var values = $("select").formSelect("getSelectedValues");
     // This prints the values array to the console
     console.log(values);
 
-<<<<<<< HEAD
-})
+    $("#datepicker").datepicker({
+        format: "yyyy/mm/dd"
+    });
 
-
-$("#datepicker").datepicker({
-format: "yyyy/mm/dd"
-});
-
-
-=======
     // This initializes the use of the modals for images
     $('.modal').modal();
 })
->>>>>>> 0b9cc490311191e6b3ee3e031c147a20673eeadc
