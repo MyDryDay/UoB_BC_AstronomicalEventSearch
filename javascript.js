6e8eba07f7ad066ce2b4e8741f721593625133e0
George Cope <G.Cope13@gmail.com>
2021-01-30 15:31:34 +0000

    Removed uneccessary jQuery link, added CSS rule for .modal-img to center the image inside the modal, & called the GeoCoding & IPGeolocation Astronomy APIs via AJAX

--- javascript.js
+++ javascript.js
@@ -14,4 +14,40 @@ $(document).ready(function(){
     $('.modal').modal();
 })
 
+$("#locationQuery").on("keypress", function(e){
+    if(e.key === "Enter"){
+        var q = $("#locationQuery").val();
+        q = q.replace(/\s/g, "");
+        console.log(q);
+        buildGeoCodeURL(q);
+    }
+});
 
+var buildGeoCodeURL = function(q){
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + q + "&appid=c7fb2f80502825ecbe90a5fece0767e4";
+    // q = "city, country code" - birmingham, gb 
+    console.log(queryURL);
+    ajaxCall(queryURL);
+}
+
+var ajaxCall = function(queryURL){
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(serverResponse){
+        console.log(serverResponse);
+        var lat = serverResponse[0].lat;
+        var lon = serverResponse[0].lon;
+        console.log(lat);
+        console.log(lon);
+
+        var queryURL2 = "https://api.ipgeolocation.io/astronomy?apiKey=ae017480db3c4202bb6465acbc3845b1&lat=" + lat + "&long=" + lon;
+        
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function(serverResponse){
+            console.log(serverResponse);
+        })
+    })
+}
